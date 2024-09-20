aec43f4d01815328022dde372b4668f4c6c657b0
Jack Angelo <3687036+dev-angelo-c@users.noreply.github.com>
2024-09-20 14:13:42 -0400

    Adds date fn

--- small_fns.js
+++ small_fns.js
@@ -3,3 +3,11 @@
 // Paste into Console
 
 let parsed_transcripts = Array.from(document.querySelectorAll(".segment")).map( i => i.innerText )[0]
+
+
+const buildDate = () => {
+  const versionBuildDate = new Date();
+  return (
+    `${versionBuildDate.getMonth()}/${versionBuildDate.getDay()}/${versionBuildDate.getFullYear()}`
+  );
+}
